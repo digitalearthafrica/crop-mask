7f3e16f8b311ed104ccc6474ad743880edef2dd3
jinjun sun <jijun.sun@ga.gov.au>
2021-03-15 14:11:55 +1100

    remove the client

--- dea-ml/dea_ml/core/cm_prediction.py
+++ dea-ml/dea_ml/core/cm_prediction.py
@@ -1,7 +1,9 @@
 import dask.array as da
-import joblib
+
+# import joblib
 import xarray as xr
-from dask.distributed import Client
+
+# from dask.distributed import Client
 from dask_ml.wrappers import ParallelPostFit
 from datacube.utils.geometry import assign_crs
 
@@ -62,101 +64,99 @@ def predict_xr(
     # convert model to dask predict
     model = ParallelPostFit(model)
 
-    _ = Client(processes=False)
-    with joblib.parallel_backend("dask"):
-        x, y, crs = input_xr.x, input_xr.y, input_xr.geobox.crs
+    # _ = Client(processes=False)
+    # with joblib.parallel_backend("dask"):
+    x, y, crs = input_xr.x, input_xr.y, input_xr.geobox.crs
 
-        input_data = []
+    input_data = []
 
-        for var_name in input_xr.data_vars:
-            input_data.append(input_xr[var_name])
+    for var_name in input_xr.data_vars:
+        input_data.append(input_xr[var_name])
 
-        input_data_flattened = []
-        # TODO: transfer to dask dataframe
-        for arr in input_data:
-            data = arr.data.flatten().rechunk(chunk_size)
-            input_data_flattened.append(data)
+    input_data_flattened = []
+    # TODO: transfer to dask dataframe
+    for arr in input_data:
+        data = arr.data.flatten().rechunk(chunk_size)
+        input_data_flattened.append(data)
 
-        # reshape for prediction
-        input_data_flattened = da.array(input_data_flattened).transpose()
+    # reshape for prediction
+    input_data_flattened = da.array(input_data_flattened).transpose()
+
+    if clean:
+        input_data_flattened = da.where(
+            da.isfinite(input_data_flattened), input_data_flattened, 0
+        )
+
+    if proba and persist:
+        # persisting data so we don't require loading all the data twice
+        input_data_flattened = input_data_flattened.persist()
+
+    # apply the classification
+    print("   predicting...")
+    out_class = model.predict(input_data_flattened)
+
+    # Mask out NaN or Inf values in results
+    if clean:
+        out_class = da.where(da.isfinite(out_class), out_class, 0)
+
+    # Reshape when writing out
+    out_class = out_class.reshape(len(y), len(x))
+
+    # stack back into xarray
+    output_xr = xr.DataArray(out_class, coords={"x": x, "y": y}, dims=["y", "x"])
+
+    output_xr = output_xr.to_dataset(name="Predictions")
+
+    if proba:
+        print("   probabilities...")
+        out_proba = model.predict_proba(input_data_flattened)
+
+        # convert to %
+        out_proba = da.max(out_proba, axis=1) * 100.0
 
         if clean:
-            input_data_flattened = da.where(
-                da.isfinite(input_data_flattened), input_data_flattened, 0
-            )
+            out_proba = da.where(da.isfinite(out_proba), out_proba, 0)
 
-        if proba and persist:
-            # persisting data so we don't require loading all the data twice
-            input_data_flattened = input_data_flattened.persist()
+        out_proba = out_proba.reshape(len(y), len(x))
 
-        # apply the classification
-        print("   predicting...")
-        out_class = model.predict(input_data_flattened)
+        out_proba = xr.DataArray(out_proba, coords={"x": x, "y": y}, dims=["y", "x"])
+        output_xr["Probabilities"] = out_proba
 
-        # Mask out NaN or Inf values in results
-        if clean:
-            out_class = da.where(da.isfinite(out_class), out_class, 0)
+    if return_input:
+        print("   input features...")
+        # unflatten the input_data_flattened array and append
+        # to the output_xr containin the predictions
+        arr = input_xr.to_array()
+        stacked = arr.stack(z=["y", "x"])
+        # handle multivariable output
+        output_px_shape = ()
+        if len(input_data_flattened.shape[1:]):
+            output_px_shape = input_data_flattened.shape[1:]
 
-        # Reshape when writing out
-        out_class = out_class.reshape(len(y), len(x))
+        output_features = input_data_flattened.reshape(
+            (len(stacked.z), *output_px_shape)
+        )
 
-        # stack back into xarray
-        output_xr = xr.DataArray(out_class, coords={"x": x, "y": y}, dims=["y", "x"])
+        # set the stacked coordinate to match the input
+        output_features = xr.DataArray(
+            output_features,
+            coords={"z": stacked["z"]},
+            dims=[
+                "z",
+                *["output_dim_" + str(idx) for idx in range(len(output_px_shape))],
+            ],
+        ).unstack()
 
-        output_xr = output_xr.to_dataset(name="Predictions")
+        # convert to dataset and rename arrays
+        output_features = output_features.to_dataset(dim="output_dim_0")
+        data_vars = list(input_xr.data_vars)
+        output_features = output_features.rename(
+            {
+                i: j for i, j in zip(output_features.data_vars, data_vars)
+            }  # noqa pylint: disable=unnecessary-comprehension
+        )
 
-        if proba:
-            print("   probabilities...")
-            out_proba = model.predict_proba(input_data_flattened)
-
-            # convert to %
-            out_proba = da.max(out_proba, axis=1) * 100.0
-
-            if clean:
-                out_proba = da.where(da.isfinite(out_proba), out_proba, 0)
-
-            out_proba = out_proba.reshape(len(y), len(x))
-
-            out_proba = xr.DataArray(
-                out_proba, coords={"x": x, "y": y}, dims=["y", "x"]
-            )
-            output_xr["Probabilities"] = out_proba
-
-        if return_input:
-            print("   input features...")
-            # unflatten the input_data_flattened array and append
-            # to the output_xr containin the predictions
-            arr = input_xr.to_array()
-            stacked = arr.stack(z=["y", "x"])
-            # handle multivariable output
-            output_px_shape = ()
-            if len(input_data_flattened.shape[1:]):
-                output_px_shape = input_data_flattened.shape[1:]
-
-            output_features = input_data_flattened.reshape(
-                (len(stacked.z), *output_px_shape)
-            )
-
-            # set the stacked coordinate to match the input
-            output_features = xr.DataArray(
-                output_features,
-                coords={"z": stacked["z"]},
-                dims=[
-                    "z",
-                    *["output_dim_" + str(idx) for idx in range(len(output_px_shape))],
-                ],
-            ).unstack()
-
-            # convert to dataset and rename arrays
-            output_features = output_features.to_dataset(dim="output_dim_0")
-            data_vars = list(input_xr.data_vars)
-            output_features = output_features.rename(
-                {
-                    i: j for i, j in zip(output_features.data_vars, data_vars)
-                }  # noqa pylint: disable=unnecessary-comprehension
-            )
-
-            # merge with predictions
-            output_xr = xr.merge([output_xr, output_features], compat="override")
+        # merge with predictions
+        output_xr = xr.merge([output_xr, output_features], compat="override")
 
     return assign_crs(output_xr, str(crs))
