1adfb36c63325c5a22a359c5c8101f403c4d615e
jinjun sun <jijun.sun@ga.gov.au>
2021-03-16 13:16:34 +1100

    update client

--- dea-ml/dea_ml/helpers/simple_runner.py
+++ dea-ml/dea_ml/helpers/simple_runner.py
@@ -7,7 +7,8 @@ import os.path as osp
 import time
 
 import psutil
-from dask.distributed import Client, LocalCluster
+
+# from dask.distributed import Client, LocalCluster
 from odc.io.cgroups import get_cpu_quota, get_mem_quota
 from odc.stats._cli_common import setup_logging
 
@@ -61,27 +62,25 @@ my_env["PYTHONPATH"] = CWD
 tasks = ["x+029/y+000/2019-P6M", "x+048/y+010"]
 
 # n_workers=1, threads_per_worker=nthreads, processes=True, memory_limit=memory_limit
-with LocalCluster(
-    n_workers=1,
-    threads_per_worker=int(0.9 * nthreads),
-    processes=False,
-    memory_limit=memory_limit,
-) as cluster, Client(cluster) as client:
-    for task in tasks:
-        tile_indx = "/".join(task.split("/")[:2])
+# with LocalCluster(
+#     n_workers=1,
+#     threads_per_worker=int(0.9 * nthreads),
+#     processes=False,
+#     memory_limit=memory_limit,
+# ) as cluster, Client(cluster) as client:
+worker = PredictFromFeature()
+for task in tasks:
+    tile_indx = "/".join(task.split("/")[:2])
 
-        file_prefix = f"{tile_indx}"
-        output_path = osp.join(output_fld, file_prefix, "*")
-        if glob.glob(output_path):
-            _log.warning(f"tile {output_path} is done already. Skipping...")
-            continue
-        _log.info(f"proessing tiles for task {output_path}. (2019-01 and 2019-07)")
+    file_prefix = f"{tile_indx}"
+    output_path = osp.join(output_fld, file_prefix, "*")
+    if glob.glob(output_path):
+        _log.warning(f"tile {output_path} is done already. Skipping...")
+        continue
+    _log.info(f"proessing tiles for task {output_path}.")
 
-        t0 = time.time()
-
-        worker = PredictFromFeature()
-        worker.run(task)
-
-        t1 = time.time()
-        wall_time = (t1 - t0) / 60
-        _log.info(f"time used {wall_time:.4f}")
+    t0 = time.time()
+    worker.run(task)
+    t1 = time.time()
+    wall_time = (t1 - t0) / 60
+    _log.info(f"time used {wall_time:.4f}")
