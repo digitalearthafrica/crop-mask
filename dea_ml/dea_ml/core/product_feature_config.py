5beb681123f06412260a14a60710596a6a4f335d
jinjun sun <jijun.sun@ga.gov.au>
2021-03-22 22:02:34 +1100

    add github file download function

--- dea_ml/dea_ml/core/product_feature_config.py
+++ dea_ml/dea_ml/core/product_feature_config.py
@@ -1,6 +1,7 @@
 import os.path as osp
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Optional
 
+import requests
 from dataclasses import dataclass
 from odc.stats.model import DateTimeRange, OutputProduct
 
@@ -123,9 +124,9 @@ def prepare_the_io_path(
     use sandbox local path to mimic the target s3 prefixes. The path follow our nameing rule:
     <product_name>/version/<x>/<y>/<year>/<product_name>_<x>_<y>_<timeperiod>_<band>.<extension>
     the name in config a crop_mask_eastern_product.yaml and the github repo for those proudct config
-    @param config: configureation dataclass as the FeaturePathConfig
-    @param tile_indx: <x>/<y>
-    @return:
+    :param config: configureation dataclass as the FeaturePathConfig
+    :param tile_indx: <x>/<y>
+    :return:
     """
 
     start_year = config.datetime_range.start.year
@@ -159,3 +160,23 @@ def prepare_the_io_path(
     each measurement a tif file. pls check the stac json schema requirements."
 
     return output_fld, paths, metadata_path
+
+
+def download_file(url: str, local_filename: Optional[str] = None) -> str:
+    """
+    Download file on github with ```?raw=true```
+    https://github....gm_mads_two_seasons_ml_model_20210301.joblib?raw=true
+    :param url:
+    :param local_filename:
+    :return:
+    """
+    local_filename = (
+        local_filename if local_filename else osp.join("/tmp", url.split("/")[-1])
+    )
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
