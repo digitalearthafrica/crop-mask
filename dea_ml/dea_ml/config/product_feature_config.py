b83b5a0b4daff7a34209f8d1dfb6ccc35342a8c1
jinjun sun <jijun.sun@ga.gov.au>
2021-03-29 14:02:31 +1100

    update the config print

--- dea_ml/dea_ml/config/product_feature_config.py
+++ dea_ml/dea_ml/config/product_feature_config.py
@@ -115,5 +115,5 @@ class FeaturePathConfig:
         href=f"https://explorer.digitalearth.africa/products/{PRODUCT_NAME}",
     )
 
-    def __repr__(self):
+    def __str__(self):
         return f"<{self.PRODUCT_NAME}>.<{self.PRODUCT_VERSION}>"
