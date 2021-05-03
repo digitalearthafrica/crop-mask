fe8eeba5ddb356bc6b94acea7d7eec1fb281e256
vfive <jsunster@gmail.com>
2021-05-03 11:07:23 +1000

    quick fix
    resolve the conflict

--- production/dea_ml/dea_ml/core/africa_geobox.py
+++ production/dea_ml/dea_ml/core/africa_geobox.py
@@ -11,21 +11,9 @@ class AfricaGeobox:
     y_new = y_old + 77
     """
 
-<<<<<<< HEAD
-    def __init__(self, resolution: Tuple[int, int] = (-10, 10), crs: str = "epsg:6933"):
-        target_crs = CRS(crs)
-        self.albers_africa_N = GridSpec(
-            crs=target_crs,
-            tile_size=(96_000.0, 96_000.0),  # default
-            resolution=resolution,
-            origin=(-7392000, -17376000),
-        )
-        africa = box(-18, -38, 60, 30, "epsg:4326")
-        self.africa_projected = africa.to_crs(crs, resolution=math.inf)
-=======
+
     def __init__(self, resolution: int = 10):
         self.albers_africa_N = GRIDS[f"africa_{resolution}"]
->>>>>>> b8fe78d... update gm mads
 
     def __getitem__(self, tile_index: Tuple[int, int]) -> GeoBox:
         return self.albers_africa_N.tile_geobox(tile_index)
